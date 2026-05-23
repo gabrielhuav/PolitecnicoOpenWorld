1b6436ea1792a4406dbd285c89ab40a36ed0127d
karrrla <karlasofia35@hotmail.com>
2026-05-22 19:40:47 -0600

    feat: agregar nuevas zonas de teletransporte al TeleportCatalog

--- PolitecnicoOpenWorld/app/src/main/java/ovh/gabrielhuav/pow/domain/models/TeleportCatalog.kt
+++ PolitecnicoOpenWorld/app/src/main/java/ovh/gabrielhuav/pow/domain/models/TeleportCatalog.kt
@@ -13,6 +13,10 @@ object TeleportCatalog {
         TeleportZone("ESCOM", 19.504505, -99.146911),
         TeleportZone("Plaza Torres", 19.506750, -99.144139),
         TeleportZone("FES ARAGON", 19.475167, -99.047444),
-        TeleportZone("Coyote de Nezahualcóyotl", 19.399806, -99.028167)
+        TeleportZone("Coyote de Nezahualcóyotl", 19.399806, -99.028167),
+        TeleportZone("UPIICSA", 19.404722, -99.094167),
+        TeleportZone("CECyT 9", 19.500833, -99.148056),
+        TeleportZone("Unidad Profesional Zacatenco", 19.504722, -99.133056),
+        TeleportZone("ESIA Zacatenco", 19.506111, -99.131389)
     )
 }
