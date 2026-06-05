fb7825eaa08b08e8d14ac6547e8a046128dcf055
Gabriel Hurtado <gabrielhurtadoaviles@gmail.com>
2026-06-05 02:31:59 -0600

    Update build.gradle.kts

--- PolitecnicoOpenWorld/app/build.gradle.kts
+++ PolitecnicoOpenWorld/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ovh.gabrielhuav.pow"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0.2"
+        versionCode = 3
+        versionName = "1.0.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
